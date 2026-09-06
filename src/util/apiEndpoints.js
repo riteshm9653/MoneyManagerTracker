310c365daa19f9b2152858fb632b3080315e6ce9
RiteshM <mritesh9653@gmail.com>
2026-09-07 01:25:04 +0530

    fronted  Development Deployment

--- src/util/apiEndpoints.js
+++ src/util/apiEndpoints.js
@@ -1,5 +1,5 @@
 // export const BASE_URL = "http://localhost:8080/api/v1.0";
-export const BASE_URL = "https://money-manager-spring-boot-backend-3.onrender.com";
+export const BASE_URL = "https://money-manager-spring-boot-backend-3.onrender.com/api/v1.0";
 
 const CLOUDINARY_CLOUD_NAME = "dl8ud6rbo";
 
