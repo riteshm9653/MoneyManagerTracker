54f44de25b37c7068a225c32f4358ce77e5a3d4c
RiteshM <mritesh9653@gmail.com>
2026-09-06 23:50:06 +0530

    Deployment frontend Development

--- src/util/apiEndpoints.js
+++ src/util/apiEndpoints.js
@@ -1,6 +1,6 @@
-export const BASE_URL = "http://localhost:8080/api/v1.0";
-// export const BASE_URL = "https://spring-backend-3-rn7b.onrender.com/api/v1.0";
-const CLOUDINARY_CLOUD_NAME = "dhadf5h7j";
+// export const BASE_URL = "http://localhost:8080/api/v1.0";
+export const BASE_URL = "https://spring-backend-3-rn7b.onrender.com/api/v1.0";
+const CLOUDINARY_CLOUD_NAME = "dl8ud6rbo";
 
 export const API_ENDPOINTS = {
     LOGIN: "/login",
